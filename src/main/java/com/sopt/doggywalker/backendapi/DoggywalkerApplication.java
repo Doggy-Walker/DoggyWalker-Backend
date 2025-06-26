d5654066982150c678de436dde7f902ef5d2318c
bingle625 <fhqhxm8899@gmail.com>
2025-06-26 15:46:32 +0900

    🎉 [init] 프로젝트 시작

---
+++ src/main/java/com/sopt/doggywalker/backendapi/DoggywalkerApplication.java
@@ -0,0 +1,13 @@
+package com.sopt.doggywalker.backendapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DoggywalkerApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(DoggywalkerApplication.class, args);
+  }
+
+}
---
+++ src/test/java/com/sopt/doggywalker/backendapi/DoggywalkerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sopt.doggywalker.backendapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DoggywalkerApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
