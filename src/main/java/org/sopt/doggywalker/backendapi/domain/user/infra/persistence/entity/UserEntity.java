51f7dfe0328b2a158926a91294beded9a31e861b
bingle625 <fhqhxm8899@gmail.com>
2025-06-26 19:59:30 +0900

    ✨ [feat] baseEntity 추가 및 jpa auditing 기능 추가
    createdAt과 updatedAt 사용

--- src/main/java/org/sopt/doggywalker/backendapi/domain/user/infra/persistence/entity/UserEntity.java
+++ src/main/java/org/sopt/doggywalker/backendapi/domain/user/infra/persistence/entity/UserEntity.java
@@ -9,14 +9,16 @@ import lombok.AccessLevel;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import org.sopt.doggywalker.backendapi.domain.user.domain.model.User;
+import org.sopt.doggywalker.backendapi.global.infra.persistence.entity.BaseEntity;
 
 @Entity
 @Table(name = "users")
 @Getter
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
-public class UserEntity {
+public class UserEntity extends BaseEntity {
 
-  @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
   private Long id;
 
   private String name;
