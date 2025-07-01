43b9c3078068f5e280ce53e3e669e3ca87c68602
bingle625 <fhqhxm8899@gmail.com>
2025-07-01 13:37:45 +0900

    🎨 [feat] Facade 클래스 이름 변경 및 역할 명확화
    - UserFacade 클래스의 이름을 UserRegisterFacade로 변경하여 역할(사용자 등록)을 더 명확하게 나타냅니다.
    
    - 기존 UserFacade는 사용자 등록 로직을 담당했지만, 클래스 이름이 포괄적이어서 오해의 소지가 있었습니다.
    
    - 이제 UserRegisterFacade는 사용자 등록 파사드 역할만 수행하도록 변경되어 코드의 가독성과 유지보수성을 향상시킵니다.

--- src/main/java/org/sopt/doggywalker/backendapi/domain/user/api/controller/UserController.java
+++ src/main/java/org/sopt/doggywalker/backendapi/domain/user/api/controller/UserController.java
@@ -2,7 +2,7 @@ package org.sopt.doggywalker.backendapi.domain.user.api.controller;
 
 import org.sopt.doggywalker.backendapi.domain.user.api.dto.request.CreateUserRequest;
 import org.sopt.doggywalker.backendapi.domain.user.api.dto.response.CreateUserResponse;
-import org.sopt.doggywalker.backendapi.domain.user.application.facade.UserFacade;
+import org.sopt.doggywalker.backendapi.domain.user.application.facade.command.UserRegisterFacade;
 import org.sopt.doggywalker.backendapi.global.response.ApiResponse;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -18,14 +18,14 @@ import lombok.RequiredArgsConstructor;
 @RequestMapping("/api/v1/users")
 public class UserController {
 
-	private final UserFacade userFacade;
+	private final UserRegisterFacade userRegisterFacade;
 
 	@PostMapping
 	public ResponseEntity<ApiResponse<CreateUserResponse>> create(
 		@RequestBody @Valid CreateUserRequest createUserRequest) {
 
 		final CreateUserResponse response = CreateUserResponse.from(
-			userFacade.register(createUserRequest.toServiceRequest()));
+			userRegisterFacade.execute(createUserRequest.toServiceRequest()));
 
 		return ResponseEntity.ok(ApiResponse.success(response));
 	}
