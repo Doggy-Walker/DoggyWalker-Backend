58e8538160c656345eaea30c206d84397a08acbf
bingle625 <fhqhxm8899@gmail.com>
2025-07-09 03:24:57 +0900

    [add] category_option, category 엔티티 추가

---
+++ src/main/java/org/sopt/pawkey/backendapi/domain/category/infra/persistence/entity/CategoryEntity.java
@@ -0,0 +1,36 @@
+package org.sopt.pawkey.backendapi.domain.category.infra.persistence.entity;
+
+import org.locationtech.jts.geom.Polygon;
+import org.sopt.pawkey.backendapi.domain.region.domain.model.RegionType;
+import org.sopt.pawkey.backendapi.global.infra.persistence.entity.BaseEntity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "category")
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor(access = AccessLevel.PROTECTED)
+@Builder(access = AccessLevel.PROTECTED)
+public class CategoryEntity extends BaseEntity {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(name = "category_id")
+	private Long category_id;
+
+	@Column(name = "category_name", nullable = false)
+	private String category_name;
+}
---
+++ src/main/java/org/sopt/pawkey/backendapi/domain/category/infra/persistence/entity/CategoryOptionEntity.java
@@ -0,0 +1,38 @@
+package org.sopt.pawkey.backendapi.domain.category.infra.persistence.entity;
+
+import org.locationtech.jts.geom.Polygon;
+import org.sopt.pawkey.backendapi.domain.region.domain.model.RegionType;
+import org.sopt.pawkey.backendapi.global.infra.persistence.entity.BaseEntity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "category_option")
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor(access = AccessLevel.PROTECTED)
+@Builder(access = AccessLevel.PROTECTED)
+public class CategoryOptionEntity extends BaseEntity {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(name = "id")
+	private Long id;
+
+	// category_id
+
+	@Column(name = "option_text", nullable = false)
+	private String option_text;
+}
