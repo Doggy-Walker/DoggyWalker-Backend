06053cad943bf283911b1a41d6ac6cea99d9ffda
daeunhan <daeunhan0723@gmail.com>
2025-07-08 16:33:27 +0900

    [docs] Swagger @Content mediaType 명시 추가

--- src/main/java/org/sopt/doggywalker/backendapi/domain/user/api/controller/UserController.java
+++ src/main/java/org/sopt/doggywalker/backendapi/domain/user/api/controller/UserController.java
@@ -28,10 +28,9 @@ public class UserController {
 
 	@Operation(summary = "유저 생성", description = "회원가입 또는 유저 등록 API입니다.", tags = {"User"})
 	@ApiResponses({
-		@io.swagger.v3.oas.annotations.responses.ApiResponse(responseCode = "200", description = "유저 생성 성공", content = @Content(mediaType = "application/json", schema = @Schema(implementation = CreateUserResponse.class))),
-		@io.swagger.v3.oas.annotations.responses.ApiResponse(responseCode = "400", description = "잘못된 요청", content = @Content),
-		@io.swagger.v3.oas.annotations.responses.ApiResponse(responseCode = "500", description = "서버 내부 오류", content = @Content)
-	})
+		@io.swagger.v3.oas.annotations.responses.ApiResponse(responseCode = "200", description = "유저 생성 성공"),
+		@io.swagger.v3.oas.annotations.responses.ApiResponse(responseCode = "400", description = "잘못된 요청", content = @Content(mediaType = "application/json")),
+		@io.swagger.v3.oas.annotations.responses.ApiResponse(responseCode = "500", description = "서버 내부 오류", content = @Content(mediaType = "application/json"))})
 	@PostMapping
 	public ResponseEntity<ApiResponse<CreateUserResponse>> create(
 		@RequestBody @Valid CreateUserRequest createUserRequest) {
