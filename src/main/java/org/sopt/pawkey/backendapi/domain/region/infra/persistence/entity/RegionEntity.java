c932d6add8b204ecc118683510a20dd0e301bcff
bingle625 <fhqhxm8899@gmail.com>
2025-07-09 02:27:44 +0900

    feat: regions 테이블에 geometry 타입 컬럼 추가

--- src/main/java/org/sopt/pawkey/backendapi/domain/region/infra/persistence/entity/RegionEntity.java
+++ src/main/java/org/sopt/pawkey/backendapi/domain/region/infra/persistence/entity/RegionEntity.java
@@ -2,6 +2,7 @@ package org.sopt.pawkey.backendapi.domain.region.infra.persistence.entity;
 
 import javax.swing.plaf.synth.Region;
 
+import org.locationtech.jts.geom.Polygon;
 import org.sopt.pawkey.backendapi.domain.region.domain.model.RegionType;
 import org.sopt.pawkey.backendapi.global.infra.persistence.entity.BaseEntity;
 
@@ -9,12 +10,9 @@ import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.EnumType;
 import jakarta.persistence.Enumerated;
-import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
-import jakarta.persistence.JoinColumn;
-import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Table;
 import lombok.AccessLevel;
 import lombok.AllArgsConstructor;
@@ -41,11 +39,11 @@ public class RegionEntity extends BaseEntity {
 
 	@Column(name = "region_name", nullable = false)
 	private String regionName;
-	//
+
 	// @ManyToOne(fetch = FetchType.LAZY)
 	// @JoinColumn(name = "parent_id")
 	// private Region parent;
 
-	// @Column(name = "area_geometry", columnDefinition = "geometry(Polygon, 4326)")
-	// private Polygon areaGeometry;
+	@Column(name = "area_geometry", columnDefinition = "geometry(Polygon, 4326)")
+	private Polygon areaGeometry;
 }
