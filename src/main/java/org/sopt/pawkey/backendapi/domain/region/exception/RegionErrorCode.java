e578999bab20975e83e948065d8b32fcd5e3c978
bingle625 <fhqhxm8899@gmail.com>
2025-07-11 17:41:19 +0900

    [refactor] typo 수정

--- src/main/java/org/sopt/pawkey/backendapi/domain/region/exception/RegionErrorCode.java
+++ src/main/java/org/sopt/pawkey/backendapi/domain/region/exception/RegionErrorCode.java
@@ -9,7 +9,7 @@ import lombok.RequiredArgsConstructor;
 @RequiredArgsConstructor(access = AccessLevel.PROTECTED)
 public enum RegionErrorCode implements ErrorCode {
 
-	REGION_NOT_FOUND(HttpStatus.NOT_FOUND, "U40401", "해당 지역을 찾을 수 없습니다.");
+	REGION_NOT_FOUND(HttpStatus.NOT_FOUND, "R40401", "해당 지역을 찾을 수 없습니다.");
 
 	private final HttpStatus status;
 	private final String code;
