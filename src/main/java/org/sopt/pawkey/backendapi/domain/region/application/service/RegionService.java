55d654ac734ff9e6653e4b17eb5061866da3ddb2
bingle625 <fhqhxm8899@gmail.com>
2025-07-11 17:37:50 +0900

    [refactor] 변수명 수정

--- src/main/java/org/sopt/pawkey/backendapi/domain/region/application/service/RegionService.java
+++ src/main/java/org/sopt/pawkey/backendapi/domain/region/application/service/RegionService.java
@@ -3,5 +3,5 @@ package org.sopt.pawkey.backendapi.domain.region.application.service;
 import org.sopt.pawkey.backendapi.domain.region.domain.model.Region;
 
 public interface RegionService {
-	Region getRegionById(Long aLong);
+	Region getRegionById(Long regionId);
 }
