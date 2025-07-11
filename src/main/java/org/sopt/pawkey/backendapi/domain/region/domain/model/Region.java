641da77aa9b033680c8cfe9098869f52ab9c26d8
daeunhan <daeunhan0723@gmail.com>
2025-07-12 02:48:57 +0900

    [fix] regionType의 타입 변경

--- src/main/java/org/sopt/pawkey/backendapi/domain/region/domain/model/Region.java
+++ src/main/java/org/sopt/pawkey/backendapi/domain/region/domain/model/Region.java
@@ -6,11 +6,11 @@ import lombok.Getter;
 public class Region {
 
 	private final Long id;
-	private final Enum regionType;
+	private final RegionType regionType;
 	private final String regionName;
 	private final Long parentId;
 
-	protected Region(Long id, Enum regionType, String regionName, Long parentId) {
+	protected Region(Long id, RegionType regionType, String regionName, Long parentId) {
 		this.id = id;
 		this.regionType = regionType;
 		this.regionName = regionName;
